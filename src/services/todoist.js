fedc684b01b47afec2308526d2ab582f8ef86105
Fuka Nagata <fleur0121@icloud.com>
2024-03-01 17:42:28 -0800

    nothing is changed

--- src/services/todoist.js
+++ src/services/todoist.js
@@ -3,16 +3,16 @@ import env from 'src/utils/env';
 import { generateRandomString } from 'src/utils/string';
 
 const todoistAPI = axios.create({
-  baseURL: 'https://api.todoist.com/rest/v2'
+	baseURL: 'https://api.todoist.com/rest/v2'
 });
 
 todoistAPI.interceptors.request.use(config => {
-		const AUTH_TOKEN = localStorage.getItem('todoistToken');
-		if (AUTH_TOKEN) {
-			config.headers.Authorization = `Bearer ${AUTH_TOKEN}`;
-		}
-		return config;
-	},
+	const AUTH_TOKEN = localStorage.getItem('todoistToken');
+	if (AUTH_TOKEN) {
+		config.headers.Authorization = `Bearer ${AUTH_TOKEN}`;
+	}
+	return config;
+},
 	error => Promise.reject(error)
 );
 
@@ -26,14 +26,47 @@ export const getAuth = async () => {
 	location.href = `https://todoist.com/oauth/authorize?client_id=${env.TODOIST_CLIENT_ID}&scope=task:add,data:read,data:read_write,data:delete,project:delete&state=${randomStr}`;
 };
 
+
+// Projects
+
 export const getProjects = () => todoistAPI.get('/projects');
 
-export const addProject = (projectName) => todoistAPI.post('/projects', { name: projectName });
+export const getProject = projectId => todoistAPI.get(`/projects/${projectId}`);
 
-export const addSubproject = (projectName, parentId) => todoistAPI.post('/projects', {name: projectName, parent_id: parentId});
+export const updateProject = (projectId, projectNewName) => todoistAPI.post(`/projects/${projectId}`, { name: projectNewName });
 
-export const addSection = (sectionName, projectId) => todoistAPI.post('/sections',{name: sectionName, project_id: projectId});
+export const addProject = projectName => todoistAPI.post('/projects', { name: projectName });
 
-export const addTask = (taskName, projectId, sectionId) => todoistAPI.post('/tasks', {content: taskName, project_id: projectId,	section_id: sectionId});
+export const addSubproject = (projectName, parentId) => todoistAPI.post('/projects', { name: projectName, parent_id: parentId });
 
-export const addSubtask = (subtaskName, projectId, sectionId, parentId) => todoistAPI.post('/tasks', {content: subtaskName,	project_id: projectId, section_id: sectionId,	parent_id: parentId});
+export const deleteProject = projectId => todoistAPI.delete(`/projects/${projectId}`);
+
+
+// Sections
+
+export const getSection = sectionId => todoistAPI.get(`/sections/${sectionId}`);
+
+export const updateSection = (sectionId, sectionNewName) => todoistAPI.post(`/sections/${sectionId}`, { name: sectionNewName });
+
+export const addSection = (sectionName, projectId) => todoistAPI.post('/sections', { name: sectionName, project_id: projectId });
+
+export const deleteSection = sectionId => todoistAPI.delete(`/sections/${sectionId}`);
+
+
+// Tasks
+
+export const getActiveTasks = (projectId, sectionId) => todoistAPI.post('/tasks', { project_id: projectId, section_id: sectionId });
+
+export const getActiveTask = taskId => todoistAPI.get(`/tasks/${taskId}`);
+
+export const updateTask = (taskId, taskNewName) => todoistAPI.post(`/tasks/${taskId}`, { content: taskNewName });
+
+export const closeTask = taskId => todoistAPI.post(`/tasks/${taskId}/close`);
+
+export const reopenTask = taskId => todoistAPI.post(`/tasks/${taskId}/reopen`);
+
+export const addTask = (taskName, projectId, sectionId) => todoistAPI.post('/tasks', { content: taskName, project_id: projectId, section_id: sectionId });
+
+export const addSubtask = (subtaskName, projectId, sectionId, parentId) => todoistAPI.post('/tasks', { content: subtaskName, project_id: projectId, section_id: sectionId, parent_id: parentId });
+
+export const deleteTask = taskId => todoistAPI.delete(`/tasks/${taskId}`);
