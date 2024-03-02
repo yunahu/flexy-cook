41be883ae749a23b2c775ae43cd008ec3279df5c
simran-kaurbrar <brars8894@gmail.com>
2024-03-01 19:55:07 -0800

    navbar added

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,20 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import NavbarBootstrap from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 const Navbar = () => {
+  const handleSearch = (event) => {
+    event.preventDefault();
+    // Handle search logic here
+  };
+
   return (
     <NavbarBootstrap expand="lg" className="bg-body-tertiary">
       <Container>
@@ -12,19 +22,27 @@ const Navbar = () => {
         <NavbarBootstrap.Toggle aria-controls="basic-navbar-nav" />
         <NavbarBootstrap.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-						<Nav.Link as={Link} to="/categories">Categories</Nav.Link>
-						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <Nav.Link as={Link} to="/categories">Categories</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
           </Nav>
+          <Form onSubmit={handleSearch} className="d-flex">
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <Button variant="outline-secondary" type="submit">Search</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
         </NavbarBootstrap.Collapse>
       </Container>
     </NavbarBootstrap>
