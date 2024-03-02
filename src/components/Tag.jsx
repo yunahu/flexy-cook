effe1b50f61bcae925f40e2ed3a3e76b3cf84bac
Fuka Nagata <fleur0121@icloud.com>
2024-03-01 17:25:31 -0800

    added tag component

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,11 +1,12 @@
 import Badge from 'react-bootstrap/Badge';
 
-const Tag = ({ title, bg = "primary" }) => {
+const Tag = ({ title, bg = "primary", className = "class"}) => {
   return (
-    <Badge pill bg={bg}>
+    <Badge pill bg={bg} className={className}> 
       {title}
     </Badge>
   );
 };
+
 
 export default Tag;
