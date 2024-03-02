1e4f48b3d042ef428cc6d33f05da9715eaf78f50
simran-kaurbrar <brars8894@gmail.com>
2024-03-01 19:56:12 -0800

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'src/App.css';
 import Router from 'src/Router';
 import Navbar from 'src/components/Navbar';
 import Footer from 'src/components/Footer';
+
+
+
 
 const App = () => {
   return (
