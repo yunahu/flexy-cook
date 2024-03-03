17cfb1a3af7eeeb117da47e4147c8b19a68bb94d
Fuka Nagata <fleur0121@icloud.com>
2024-03-02 21:39:19 -0800

    #25 pass information to the large card

---
+++ src/components/Cards/CookingInfo/CookingInfo.jsx
@@ -0,0 +1,35 @@
+import Stack from "react-bootstrap/Stack";
+
+import { Container } from "react-bootstrap";
+import styles from "./CookingInfo.module.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUtensils, faClockRotateLeft, faKitchenSet } from "@fortawesome/free-solid-svg-icons";
+
+const CookingInfo = ({ size, time, calories }) => {
+  /*
+      info {
+         dishType: 'Oven',
+         time: 40,
+         size: 2
+      }
+   */
+  return (
+    <Stack direction="horizontal" className={styles.wrapper}>
+      <Container className={styles.col}>
+        <FontAwesomeIcon icon={faKitchenSet} />
+        &ensp;{calories} kcal
+      </Container>
+      <Container className={styles.col}>
+        <FontAwesomeIcon icon={faClockRotateLeft} />
+        &ensp;{time} minutes
+      </Container>
+      <Container className={styles.col}>
+        <FontAwesomeIcon icon={faUtensils} />
+        &ensp;{size} {size > 1 ? "servings" : "serving"}
+      </Container>
+    </Stack>
+  );
+};
+
+export default CookingInfo;
