a95a861c247f05424bfba1621b872618a8a3917e
Fuka Nagata <fleur0121@icloud.com>
2024-03-09 12:30:28 -0800

    update advanced search #56

---
+++ src/pages/Home/components/HorizontalCard/HorizontalCard.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/prop-types */
+import CardBootstrap from "react-bootstrap/Card";
+import CookingInfo from "src/components/Cards/CookingInfo/CookingInfo";
+import Tags from "src/components/Cards/Tags/Tags";
+
+import styles from "./HorizontalCard.module.css";
+import { Row, Col, Stack } from "react-bootstrap";
+
+const HorizontalCard = ({
+  imgURL,
+  width,
+  title,
+  ingredients,
+  tags,
+  time,
+  size,
+  calories,
+}) => {
+  /**
+   * info: object with equip, time, size as keys
+   * tags: array of objects, each has color, text as keys
+   */
+  return (
+    <>
+      <CardBootstrap className={styles.hori_Card} style={{ width: width }}>
+        <Row style={{ height: "100%" }}>
+          <Col xs={4} className={styles.imgBlock}>
+            <CardBootstrap.Img src={imgURL} className={styles.cardImg} />
+          </Col>
+
+          <Col xs={8} className={styles.cardBody}>
+            <CardBootstrap.Body>
+              <Stack direction="vertical">
+                <CardBootstrap.Title>{title}</CardBootstrap.Title>
+
+                <CardBootstrap.Subtitle>Ingredients</CardBootstrap.Subtitle>
+                <CardBootstrap.Text>{ingredients}</CardBootstrap.Text>
+
+                <CookingInfo size={size} time={time} calories={calories} />
+                <Tags tags={tags} />
+              </Stack>
+            </CardBootstrap.Body>
+          </Col>
+        </Row>
+      </CardBootstrap>
+    </>
+  );
+};
+
+export default HorizontalCard;
