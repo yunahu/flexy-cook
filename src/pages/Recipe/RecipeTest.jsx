0d30d2d613307d4a4cf4d7fd492caadda0a58b8e
Monica Gao <mgaozh@gmail.com>
2024-03-18 12:45:00 -0700

    Roughly applied upgraded recipe page layout to  page, pending props fix.

--- src/pages/Recipe/RecipeTest.jsx
+++ src/pages/Recipe/RecipeTest.jsx
@@ -1,7 +1,14 @@
 /* eslint-disable no-unused-vars */
+import RecipeBanner from './components/RecipeBanner/RecipeBanner';
+import Divider from 'src/components/Divider/Divider';
 import CookingStep from "./components/CookingStep/CookingStep";
+import ToolBar from './components/ToolBar/ToolBar';
+
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+
+import { Stack } from 'react-bootstrap';
+import styles from './Recipe.module.css'
 
 const RecipeTest = () => {
   const location = useLocation();
@@ -53,8 +60,34 @@ const RecipeTest = () => {
 
   return (
     <>
-      <h1>Recipe Page Content Test</h1>
-      {CookingSteps}
+      {/* <h1>Recipe Page Content Test</h1>
+      {CookingSteps} */}
+
+      <Stack className={styles.wrapper} direction='vertical' gap={2}>
+         <hr id='top'></hr>
+
+         <RecipeBanner
+            imgURL={recipe.image}
+            title={recipe.title}
+            description={recipe.description}
+            time={recipe.info}
+            calories={recipe.calories}
+            size={recipe.size}
+            tags={recipe.tags}
+         />
+            
+         <Divider variant={'down'} label={'Scroll Down'} width={'100%'}/>
+            
+         <Stack className={styles.cookingStep} direction='vertical'>
+            {/* <CookingStep height={recipeInfo.height} image={recipeInfo.equipment[0].image} number={recipeInfo.number} step={recipeInfo.step} equipment={recipeInfo.equipment} ingredients={recipeInfo.ingredients}/>
+            <CookingStep height={recipeInfo.height} image={recipeInfo.equipment[0].image} number={recipeInfo.number} step={recipeInfo.step} equipment={recipeInfo.equipment} ingredients={recipeInfo.ingredients}/>
+            <CookingStep height={recipeInfo.height} image={recipeInfo.equipment[0].image} number={recipeInfo.number} step={recipeInfo.step} equipment={recipeInfo.equipment} ingredients={recipeInfo.ingredients}/> */}
+            {CookingSteps}
+         </Stack>
+
+         </Stack> {/** End Content Wrapper */}
+
+         <ToolBar className={styles.toolBar}/>
     </>
   );
 };
