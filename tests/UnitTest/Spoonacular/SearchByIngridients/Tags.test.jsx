c5b10a032fffe33c9f968aaa6afa33eb767496b3
Fuka Nagata <fleur0121@icloud.com>
2024-03-23 16:02:18 -0700

    added tags test

--- tests/UnitTest/Spoonacular/SearchByIngridients/Tags.test.jsx
+++ tests/UnitTest/Spoonacular/SearchByIngridients/Tags.test.jsx
@@ -1,8 +1,9 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Tags from "src/components/Cards/Tags/Tags";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { getTagInfo } from "src/utils/spoonacularFunctions";
 
 const mockUseNavigate = vi.fn();
 
@@ -12,6 +13,10 @@ vi.mock("react-router-dom", () => ({
 
 describe("Tags", () => {
   beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test("render successfully", async () => {
     const tags = [
       {
         key: "diet",
@@ -31,41 +36,74 @@ describe("Tags", () => {
     ];
     render(<Tags tags={tags} />);
   });
-  test("render successfully", async () => {});
 
-  test("tags have button role", async () => {
-    expect(screen.getByRole("button").textContent).toBe("Japanese");
-    expect(screen.getByRole("button").textContent).toBe("Soup");
-    expect(screen.getByRole("button").textContent).toBe("Vegan");
+  test("the tag has a button role", async () => {
+    const tags = [
+      {
+        key: "diet",
+        text: "Vegan",
+        type: "warning",
+      },
+    ];
+    render(<Tags tags={tags} />);
+    expect(screen.getAllByRole("button")).toBeInTheDocument;
   });
 
   test("texts are shown", async () => {
-    expect(screen.getByRole("button").textContent).toBe("Japanese");
-    expect(screen.getByRole("button").textContent).toBe("Soup");
-    expect(screen.getByRole("button").textContent).toBe("Vegan");
+    const tags = [
+      {
+        key: "diet",
+        text: "Vegan",
+        type: "warning",
+      },
+      {
+        key: "type",
+        text: "Soup",
+        type: "dark",
+      },
+      {
+        key: "cuisine",
+        text: "Japanese",
+        type: "info",
+      },
+    ];
+    render(<Tags tags={tags} />);
+    // text are shown?
+    expect(screen.getAllByText("Japanese")).toBeInTheDocument;
+    expect(screen.getAllByText("Soup")).toBeInTheDocument;
+    expect(screen.getAllByText("Vegan")).toBeInTheDocument;
   });
 
-  test("if disabled is true, then the Modified Button can't be clicked", async () => {
-    expect(onClickMock).toHaveBeenCalledTimes(0);
-    const button = screen.getByRole("button");
-    await userEvent.click(button);
-    expect(onClickMock).toHaveBeenCalledTimes(1);
-    await userEvent.click(button);
-    expect(onClickMock).toHaveBeenCalledTimes(2);
-    const onClickMock = vi.fn(() => {});
+  test("when the tags are clicked, onClick is called", async () => {
+    const tags = [
+      {
+        key: "diet",
+        text: "Vegan",
+        type: "warning",
+      },
+      {
+        key: "type",
+        text: "Soup",
+        type: "dark",
+      },
+      {
+        key: "cuisine",
+        text: "Japanese",
+        type: "info",
+      },
+    ];
+    render(<Tags tags={tags} />);
 
-    render(
-      <ModifiedButton
-        title="Test Button"
-        disabled={true}
-        onClick={onClickMock}
-      />
-    );
+    const JPTag = screen.getByText("Japanese");
+    // use navigate is not called yet?
+    expect(mockUseNavigate).toBeCalledTimes(0);
+    await userEvent.click(JPTag);
 
-    const button = screen.getByRole("button");
-    await userEvent.click(button);
-
-    expect(onClickMock).toHaveBeenCalledTimes(0);
-    expect(button).toBeDisabled();
+    // use navigate is not called once?
+    expect(mockUseNavigate).toBeCalledTimes(1);
+    // navigates "/testSearch" with information?
+    expect(mockUseNavigate).toBeCalledWith("/testSearch", {
+      state: { tagInfo: getTagInfo(tags[2]) },
+    });
   });
 });
