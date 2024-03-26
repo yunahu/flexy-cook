14675132e68a2a5ac1a30bfc049b660353bc625c
Fuka Nagata <fleur0121@icloud.com>
2024-03-26 00:49:30 -0700

    fixed image fetch

--- src/services/spoonacular.js
+++ src/services/spoonacular.js
@@ -2,15 +2,23 @@ export const getImage = (recipe, index) => {
     const { analyzedInstructions } = recipe;
     const steps = (analyzedInstructions[0].steps[index]);
     if (steps.equipment[0] && steps.equipment[0].image) {
-        return `https://spoonacular.com/cdn/equipment_250x250/${steps.equipment[0].image}`;
-        // 100x100 / 250x250 / 500x500
+        if ((steps.equipment[0].image).includes("https")) {
+            return steps.equipment[0].image;
+        } else {
+            return `https://spoonacular.com/cdn/equipment_250x250/${steps.equipment[0].image}`;
+            // 100x100 / 250x250 / 500x500
+        }
     } else if (
         steps.ingredients[0] &&
         (steps.ingredients[0].image || steps.ingredients[1].image)
     ) {
-        return `https://spoonacular.com/cdn/ingredients_250x250/${steps.ingredients[0].image || steps.ingredients[1].image
-            }`;
-        // 100x100 / 250x250 / 500x500
+        if ((steps.ingredients[0].image).includes("https")) {
+            return steps.ingredients[0].image
+        } else {
+            return `https://spoonacular.com/cdn/ingredients_250x250/${steps.ingredients[0].image || steps.ingredients[1].image
+                }`;
+            // 100x100 / 250x250 / 500x500
+        }
     } else {
         return recipe.image;
     }
