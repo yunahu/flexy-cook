cb9430c7c7cb081eba3753fc4d96f7bafd7bf2c5
Fuka Nagata <fleur0121@icloud.com>
2024-04-02 22:43:00 -0700

    update Navbar test

--- tests/IntegrationTest/Todoist/GeneralTodoList/Navbar.test.jsx
+++ tests/IntegrationTest/Todoist/GeneralTodoList/Navbar.test.jsx
@@ -1,38 +1,7 @@
 import { describe, test, expect, beforeEach, afterEach } from "vitest";
-<<<<<<< HEAD
-import { render, screen, waitFor } from "@testing-library/react";
-import Navbar from "src/components/Navbar/Navbar";
-import { MemoryRouter } from "react-router-dom";
-import { TodoListsContext } from "src/App";
-
-const mockUseNavigate = vi.fn();
-
-vi.mock("react-router-dom", () => ({
-  useNavigate: () => mockUseNavigate,
-}));
-vi.mock("react-router-dom", async (importOrig) => {
-  return {
-    ...(await importOrig()),
-  };
-});
-
-describe("NavBar", () => {
-  test("render successfully", async () => {
-    render(
-      <MemoryRouter initialEntries={[{ pathname: "/" }]}>
-        <TodoListsContext.Provider
-          value={{ todoLists: [], setTodoLists: vi.fn() }}
-        >
-          <Navbar />
-        </TodoListsContext.Provider>
-      </MemoryRouter>
-    );
-  });
-});
-=======
 import { fireEvent, render, waitFor } from "@testing-library/react";
-import { HttpResponse, http } from 'msw';
-import { setupServer } from 'msw/node';
+import { HttpResponse, http } from "msw";
+import { setupServer } from "msw/node";
 import { TodoListsContext } from "src/App";
 import { TODOIST_BASE_URL } from "src/services/todoist";
 import TodoistProjectsMock from "../../../mocks/Todoist/TodoistProjectsMock.json";
@@ -41,35 +10,41 @@ import Navbar from "src/components/Navbar/Navbar";
 import { MemoryRouter } from "react-router-dom";
 
 const server = setupServer(
-	http.get(`${TODOIST_BASE_URL}/projects`, () => HttpResponse.json(TodoistProjectsMock)),
-	http.get(`${TODOIST_BASE_URL}/sections`, () => HttpResponse.json(TodoistSectionsMock))
+  http.get(`${TODOIST_BASE_URL}/projects`, () =>
+    HttpResponse.json(TodoistProjectsMock)
+  ),
+  http.get(`${TODOIST_BASE_URL}/sections`, () =>
+    HttpResponse.json(TodoistSectionsMock)
+  )
 );
 
 let todoLists = [];
-const setTodoLists = x => { todoLists = x };
+const setTodoLists = (x) => {
+  todoLists = x;
+};
 const providerProps = { value: { todoLists, setTodoLists } };
 
-const customRender = (ui, renderOptions ) => {
-	const mockRoute = '/home';
+const customRender = (ui, renderOptions) => {
+  const mockRoute = "/home";
 
   return render(
-		<MemoryRouter initialEntries={[mockRoute]}>
-			<TodoListsContext.Provider {...providerProps}>
-				{ui}
-			</TodoListsContext.Provider>
-		</MemoryRouter>,
-    renderOptions,
+    <MemoryRouter initialEntries={[mockRoute]}>
+      <TodoListsContext.Provider {...providerProps}>
+        {ui}
+      </TodoListsContext.Provider>
+    </MemoryRouter>,
+    renderOptions
   );
 };
 
 beforeAll(() => {
-	localStorage.setItem("todoistToken", "TEST_TOKEN");
-	server.listen({ onUnhandledRequest: 'error' });
+  localStorage.setItem("todoistToken", "TEST_TOKEN");
+  server.listen({ onUnhandledRequest: "error" });
 });
 
-afterAll(() => { 
-	localStorage.removeItem("todoistToken");
-	server.close();
+afterAll(() => {
+  localStorage.removeItem("todoistToken");
+  server.close();
 });
 
 afterEach(() => server.resetHandlers());
@@ -78,17 +53,18 @@ beforeEach(async () => setTodoLists([]));
 
 describe("Navbar", () => {
   test("should render the Navbar", async () => {
-		const result = customRender(<Navbar/>);
-		expect(result).toBeTruthy();
+    const result = customRender(<Navbar />);
+    expect(result).toBeTruthy();
   });
-	
-	test("'Add New Memo' button should trigger the TodoListsModal to be rendered", async () => {
-		const result = customRender(<Navbar/>);
-		const TODObutton = result.container.querySelector('#TODObutton');
-		fireEvent.click(TODObutton);
-		const addNewMemoButton = result.container.querySelector('#addNewMemo');
-		fireEvent.click(addNewMemoButton);
-		await waitFor(() => expect(document.querySelector('#todoListsModal')).toBeInTheDocument());
-	});
+
+  test("'Add New Memo' button should trigger the TodoListsModal to be rendered", async () => {
+    const result = customRender(<Navbar />);
+    const TODObutton = result.container.querySelector("#TODObutton");
+    fireEvent.click(TODObutton);
+    const addNewMemoButton = result.container.querySelector("#addNewMemo");
+    fireEvent.click(addNewMemoButton);
+    await waitFor(() =>
+      expect(document.querySelector("#todoListsModal")).toBeInTheDocument()
+    );
+  });
 });
->>>>>>> development
