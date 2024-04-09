b43019250d4d98bf779687949e0ec36c4ec3119a
Yuna Hu <yunaa.765@gmail.com>
2024-04-08 18:38:56 -0700

    Fixed an error that a new user logs in with todoist account, the todo list does not create memos list and shopping list unless the page gets refreshed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { TodoListsContext } from "src/App";
+import { addSection, getFlexyCookProject, getSections } from "src/services/todoist";
 import { getQueryString } from "src/utils/common";
 import env from "src/utils/env";
 
 const Login = () => {
+	const { todoLists, setTodoLists } = useContext(TodoListsContext);
+
 	const navigate = useNavigate();
 	useEffect(() => {
 		const getTodoistToken = async () => {
@@ -20,6 +24,46 @@ const Login = () => {
 				const res = await axios.post(`${env.API_URL}/todoist_token`, {code: queryString.code});
 				localStorage.setItem('todoistToken', res.data);
 				alert('Todoist login successful.'); // TODO: Replace
+
+				const orderSections = async (sections, flexyCookProject) => {
+					const orderedSections = [];
+					let existsMemos = false;
+					let existsShoppingList = false;
+					
+					for (const section of sections) {
+						if (section.name === 'Memos') {
+							existsMemos = true;
+							orderedSections.splice(0, 0, section);
+						} else if (section.name === 'Shopping List') {
+							existsShoppingList = true;
+							if (orderedSections[0]?.name === 'Memos') {
+								orderedSections.splice(1, 0, section);
+							} else {
+								orderedSections.splice(0, 0, section);
+							};
+						} else {
+							orderedSections.push(section);
+						};
+					};
+					
+					
+					if (!existsMemos) {
+						const memos =  await addSection('Memos', flexyCookProject.id);
+						orderedSections.splice(0, 0, memos);
+					};
+	
+					if (!existsShoppingList) {
+						const shoppingList = await addSection('Shopping List', flexyCookProject.id);
+						orderedSections.splice(1, 0, shoppingList);
+					};
+	
+					return orderedSections;
+				};
+				
+				const flexyCookProject = await getFlexyCookProject();	
+				const flexyCookSections = await getSections(flexyCookProject.id);
+				const orderedSections = await orderSections(flexyCookSections, flexyCookProject);
+				setTodoLists(orderedSections);
 			} catch (err) {
 				console.error(err);
 			}
